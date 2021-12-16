83241eed8a0d1a74d17fb5b00f07c9f86b236089
Matt Ruder <matt.j.ruder@gmail.com>
2021-12-16 15:15:28 -0700

    add finalPrep

--- finalPrep/annotations.js
+++ finalPrep/annotations.js
@@ -2,10 +2,16 @@
 // Use the double // to create a new comment
 
 //Build a Bear
+// declare a function called buildABear and add 5 parameters
 function buildABear(name, age, fur, clothes, specialPower) {
+
+// declare a variable greeting that returns a string which interpolates the bear name
   var greeting = `Hey partner! My name is ${name} - will you be my friend?!`;
+//declare a variable that returns an array with the bear's name and age
   var demographics = [name, age];
+// declare a variable that returns a string that states the bear's special power
   var powerSaying = "Did you know that I can " + specialPower + " ?";
+// declare a variable assigned to an object with several key-value pairs that define the bear
   var builtBear = {
     basicInfo: demographics,
     clothes: clothes,
@@ -15,23 +21,32 @@ function buildABear(name, age, fur, clothes, specialPower) {
     isCuddly: true,
   };
 
+// returns variable builtBear and includes each key-value pair
   return builtBear
 }
 
+// defines 2 bears with different parameters.
 buildABear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares');
 buildABear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in');
 
 
 
 //FizzBuzz
+
+//declare a function called fizzBuzz containing 3 parameters
 function fizzBuzz(num1, num2, range) {
+//create a for loop with a condition counter containing initialization, condition and update
   for (var i = 0; i <= range; i++) {
+//if there is a remainder of 0 when you divide i by num1 and num2, log 'fizzbuzz'
     if (i % num1 === 0 && i % num2 === 0) {
       console.log('fizzbuzz');
+// if only num1 has a remainder of 0, log 'fizz'
     } else if (i % num1 === 0) {
       console.log('fizz');
+// if only num2 has a remainder of 0, log 'buzz'
     } else if (i % num2 === 0) {
       console.log('buzz');
+// if neither have a remainder of 0, log i
     } else {
       console.log(i);
     }
@@ -39,4 +54,4 @@ function fizzBuzz(num1, num2, range) {
 }
 
 fizzBuzz(3, 5, 100);
-fizzbuzz(5, 8, 400);
+fizzBuzz(5, 8, 400);
--- finalPrep/modZeroHero.js
+++ finalPrep/modZeroHero.js
@@ -2,39 +2,83 @@
 
 // Declare two variables - heroName AND specialAbility - set to strings
 
+var heroName = "Matt";
+var specialAbility = "Laser Eyes";
+
 // Declare two variables - greeting AND catchphrase
 //   greeting should be assigned to a string that uses concatenation to include the heroName
 //   catchphrase should be assigned to a string that uses interpolation to include the specialAbility
 
+var greeting = "Never fear, " + heroName + " is here!";
+console.log(greeting);
+
+var catchphrase = `Sorry guys, its ${specialAbility}!`
+console.log(catchphrase);
+
 // Declare two variables - power AND energy - set to integers
+
+var power = 9000;
+var energy = 75;
 
 // Declare two variables - fullPower AND fullEnergy
 //   fullPower should multiply your current power by 500
 //   fullEnergy should add 150 to your current energy
 
+var fullPower = power * 500;
+var fullEnergy = energy + 150;
+console.log(fullPower);
+console.log(fullEnergy);
+
 // Declare two variables - isHuman and identityConcealed - assigned to booleans
 
+var isHuman = true;
+var identityConcealed = false;
 
 // Declare two variables - archEnemies AND sidekicks
 //   archEnemies should be an array of at least 3 different enemy strings
 //   sidekicks should be an array of at least 3 different sidekick strings
 
+var archEnemies = ["Blob Man", "Blob Woman", "Blob Dog"];
+var sidekicks = ["Jelly Boy", "Old Boy", "Boy Boy"];
+
 // Print the first sidekick to your console
+
+console.log(sidekicks[0]);
 
 // Print the last archEnemy to the console
 
+console.log(archEnemies[2]);
+
 // Write some code to add a new archEnemy to the archEnemies array
+
+archEnemies.push("Just A Blob");
 
 // Print the archEnemies array to console to ensure you added a new archEnemy
 
+console.log(archEnemies);
+
 // Remove the first sidekick from the sidekicks array
+
+sidekicks.shift();
 
 // Print the sidekicks array to console to ensure you added a new sidekick
 
+console.log(sidekicks);
+
 // Create a function called assessSituation that takes three arguments - dangerLevel, saveTheDay, badExcuse
 //   - dangerLevel should be an integer
-//   - saveTheDay should be a string a hero would say once they save the day 
+//   - saveTheDay should be a string a hero would say once they save the day
 //   - badExcuse should be a string a hero would say if they are too afraid of the dangerLevel
+
+function assessSituation(dangerLevel, saveTheDay, badExcuse) {
+    if (dangerLevel > 50) {
+      console.log(badExcuse);
+    } else if (dangerLevel >= 10) {
+      console.log(saveTheDay);
+    } else if (dangerLevel < 10) {
+      console.log("Meh, hard pass");
+    }
+}
 
 // Your function should include an if/else statement that meets the following criteria
 //   - Danger levels that are above 50 are too scary for your hero. Any danger level that is above 50 should result in printing the badExcuse to the console
@@ -48,6 +92,10 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //assessSituation(21, announcement, excuse) > should print - 'Never fear, the Courageous Curly Bracket is here!'
 //assessSituation(3, announcement, excuse) > should print - "Meh. Hard pass."
 
+assessSituation(99, announcement, excuse);
+assessSituation(21, announcement, excuse);
+assessSituation(3, announcement, excuse);
+
 // Declare a new variable - scaryMonster - assigned to an Object with the following key/values
 //   - name (string)
 //   - smell (string)
@@ -56,16 +104,52 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - luckyNumbers (array)
 //   - address (object with following key/values: number , street , state, zip)
 
+var scaryMonster = {
+  name : "Evilboy",
+  smell : "Evil",
+  weightInLbs : 700,
+  citiesDestroyed : ["Denver", "Dallas", "Houston"],
+  luckyNumbers : [17, 27, 7],
+  address : [17, "Wallaby Way", "Texas", 75002]
+  }
+
+  console.log(scaryMonster);
+
+
 
 // Create a new class called SuperHero
 // - Your class should have the following DYNAMIC values
-//   - name 
+//   - name
 //   - superpower
-//   - age 
+//   - age
 // - Your class should have the following STATIC values
 //   - archNemesis, assigned to "The Syntax Error"
 //   - powerLevel = 100
-//   - energyLevel = 50 
+//   - energyLevel = 50
+
+class SuperHero {
+  constructor(name, superpower, age, archNemesis, powerLevel, energyLevel){
+    this.name = name;
+    this.superpower = superpower;
+    this.age = age;
+    this.archNemesis = "The Syntax Error";
+    this.powerLevel = 100;
+    this.energyLevel = 50;
+  }
+
+  sayName(){
+    console.log(this.name);
+  }
+
+  maximizeEnergy(){
+    console.log(this.energyLevel = 1000);
+  }
+
+  gainPower(addPower){
+    console.log(this.powerLevel = this.powerLevel + addPower);
+  }
+}
+
 
 // - Create the following class methods
 //   - sayName, should print the hero's name to the console
@@ -73,12 +157,42 @@ var excuse = 'I think I forgot to lock up my 1992 Toyota Coralla. Be right back.
 //   - gainPower, should take an argument of a number and INCREASE the powerLevel by that number
 
 // - Create 2 instances of your SuperHero class
+var goodBoy = new SuperHero("GoodBoy", "Slime Hands", 27);
+goodBoy.sayName();
 
+console.log(goodBoy);
+
+goodBoy.maximizeEnergy();
+
+console.log(goodBoy);
+
+goodBoy.gainPower(50);
+
+console.log(goodBoy);
+
+
+var betterBoy = new SuperHero("BetterBoy", "Slime Feet", 28);
+betterBoy.sayName();
+
+console.log(betterBoy);
+
+betterBoy.maximizeEnergy();
+
+console.log(betterBoy);
+
+betterBoy.gainPower(70);
+
+console.log(betterBoy);
 
 // Reflection
 // What parts were most difficult about this exerise?
 
+//The class methods were most difficult for me, but I got them in the end!
+
 // What parts felt most comfortable to you?
+
+// Everything except the class methods.
 
 // What skills do you need to continue to practice before starting Mod 1?
 
+// Everything, but especially class methods!
